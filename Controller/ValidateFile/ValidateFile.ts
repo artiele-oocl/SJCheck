8d27938bd44c17387dd0ad6e433cf09344526410
Lester Jan Artienda <lester.jan.artienda@oocl.com>
2019-02-20 18:53:51 +0800

    Started ValidateFile.

--- Controller/ValidateFile/ValidateFile.ts
+++ Controller/ValidateFile/ValidateFile.ts
@@ -7,7 +7,30 @@ import * as fse from 'fs-extra';
  */
 
 export class ValidateFile {
-    isFileValid(): boolean {return;}
-    getSize(): number { return;}
-    isContentValid(): boolean { return;}
+    file: string;
+    constructor(file: string) {
+        this.file = file;
+    }
+    isFileValid(): boolean {
+        return this.isContentValid();
+    }
+    isNotEmpty(): boolean {
+        return fse.statSync(this.file).size > 0;
+    }
+    isContentValid(): boolean {
+        // read content of file; check if READABLE
+        if (!this.isNotEmpty()) return false;
+        // If content is all whitespace then return false ELSE true
+        // Replace all whitespace to '' then return isNotEmpty();
+        fse.readFile(this.file, 'utf8', (err,data) => {
+            if (err) {
+                console.log(err);
+                return false;
+            }
+            console.log(data.replace(/ /g, ''));
+            console.log(this.isNotEmpty()); // not working kasi returns TRUE kahit false-postive
+        });
+
+        return;
+    }
 }
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
+import { ValidateFile } from "./Controller/ValidateFile/ValidateFile";
+
 /**
  * Accept args[0] then toLowerCase then choose config file to read
  * read config file (json) then iterate over it with DirectoryController
  * return: result.json <--which will be created to ./Result/result.json
  * Note that result.json will be process and sent via email
  */
+
+ let v = new ValidateFile('ReportDummy/false-positive/false-positive.csv');
+ v.isContentValid();
